0f4effea1b8ff9086ff62da736cf3b70efc24fdd
Deepankar Sharma <deepankar.sharma@datatailr.com>
2023-02-24 13:24:35 -0500

    Add conditional compilation for syscall because build breaks on windows

---
+++ utils/platform.go
@@ -0,0 +1,16 @@
+package lxdapi
+
+import (
+	"io/fs"
+	"syscall"
+)
+
+func GetUidGid(stat fs.FileInfo) (int64, int64, error) {
+	if linuxstat, ok := stat.Sys().(*syscall.Stat_t); ok {
+		UID := int64(linuxstat.Uid)
+		GID := int64(linuxstat.Gid)
+		return UID, GID, nil
+
+	}
+	return 0, 0, nil
+}
---
+++ utils/platform_windows.go
@@ -0,0 +1,5 @@
+package lxdapi
+
+func GetUidGid(stat fs.FileInfo) (int64, int64, error) {
+	return 0, 0, nil
+}
